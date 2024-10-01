5e029fd53bd1f590ea1a783d40bb18f292659f75
JDennis21 <114511121+JDennis21@users.noreply.github.com>
2024-10-01 19:27:46 +0100

    Unit test for PenColour()

--- ASE_AssignmentTests/AppCanvasTests.cs
+++ ASE_AssignmentTests/AppCanvasTests.cs
@@ -1,32 +1,49 @@
 using ASE_Assignment;
+using BOOSE;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using PenColour = BOOSE.PenColour;
 
 namespace ASE_AssignmentTests
 {
     [TestClass]
     public class AppCanvasTests
     {
-        private AppCanvas _appCanvas;
+        private AppCanvas _mockCanvas;
+        private StoredProgram _program;
+        private Parser _parser;
+        private ASE_Assignment.CommandFactory _factory;
 
         [TestInitialize]
         public void Setup()
         {
-            _appCanvas = new AppCanvas(800, 600);
+            _mockCanvas = new AppCanvas(800, 600);
         }
 
         [TestMethod]
         public void TestMoveTo_SetsPenPosition()
         {
-            _appCanvas.MoveTo(200, 200);
-            Assert.AreEqual(_appCanvas.Xpos, 200);
-            Assert.AreEqual(_appCanvas.Ypos, 200);
+            _mockCanvas.MoveTo(200, 200);
+            Assert.AreEqual(_mockCanvas.Xpos, 200);
+            Assert.AreEqual(_mockCanvas.Ypos, 200);
         }
 
         [TestMethod]
         public void TestDrawTo_SetsPenPosition()
         {
-            _appCanvas.DrawTo(200, 200);
-            Assert.AreEqual(_appCanvas.Xpos, 200);
-            Assert.AreEqual(_appCanvas.Ypos, 200);
+            _mockCanvas.DrawTo(200, 200);
+            Assert.AreEqual(_mockCanvas.Xpos, 200);
+            Assert.AreEqual(_mockCanvas.Ypos, 200);
+        }
+
+        [TestMethod]
+        public void TestPenColour_SetBlue()
+        {
+            _factory = new ASE_Assignment.CommandFactory();
+            _program = new StoredProgram(_mockCanvas);
+            _parser = new Parser(_factory, _program);
+
+            _parser.ParseProgram("pen 0,0,255");
+            _program.Run();
         }
     }
 }
